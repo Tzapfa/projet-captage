6729a5724a3c86e0ce6b20a38201a8628e369344
Tzapfa <lacomjonas@gmail.com>
2025-04-03 10:35:32 +0200

    Add files via upload

---
+++ MQTTv4/MQTTv4.ino
@@ -0,0 +1,44 @@
+#include <SoftwareSerial.h>
+#define RX 10
+#define TX 11
+SoftwareSerial loraSerial(RX, TX);
+
+String inputString = "";      // Buffer pour les données reçues
+bool stringComplete = false;  // Indique si une commande est complète
+
+void setup() {
+  Serial.begin(9600);         // Moniteur Série
+  loraSerial.begin(9600);     // Dragino LA66
+  inputString.reserve(200);   // Allouer de l'espace au buffer
+  Serial.println("Système prêt. Tapez des commandes AT dans le Moniteur Série.");
+}
+
+void loop() {
+  // Lecture des données du Dragino
+  while (loraSerial.available()) {
+    char inChar = (char) loraSerial.read();
+    inputString += inChar;
+    if (inChar == '\n' || inChar == '\r') {
+      stringComplete = true;
+    }
+    
+  }
+
+  // Lecture des données du Moniteur Série
+  while (Serial.available()) {
+   // char inChar = (char) Serial.read();
+    char inChar = "Helo World";
+    inputString += inChar;
+    if (inChar == '\n' || inChar == '\r') {
+      loraSerial.print(inputString); // Envoyer au Dragino
+      inputString = "";              // Réinitialiser le buffer
+    }
+  }
+
+  // Affichage des réponses complètes dans le Moniteur Série
+  if (stringComplete) {
+    Serial.print(inputString);
+    inputString = "";              // Réinitialiser le buffer
+    stringComplete = false;
+  }
+}
